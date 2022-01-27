62de56fda061715127643c250e93653a95c5cc6c
Maxim <korkinwow@gmail.com>
2022-01-27 20:28:18 +0700

    Selector instead of index in setFormErrorMessage #JC19-257

--- vanilla/src/utils/setFormErrorMessage.ts
+++ vanilla/src/utils/setFormErrorMessage.ts
@@ -1,14 +1,14 @@
 /**
  * Function for displaying a message inside the form in case of errors.
  * @param form - The form where the message should be displayed.
- * @param message - String with the message.
+ * @param message - String with the message, use empty string to remove message from the form.
  */
 export function setFormErrorMessage(form: HTMLFormElement, message: string): void {
   if (message.trim() !== '') {
     const messageElement = document.createElement('div');
     messageElement.classList.add('form-error-message');
     messageElement.innerHTML = `<p>${message}</p>`;
-    form.insertBefore(messageElement, form.children[2]);
+    form.insertBefore(messageElement, form.querySelector('btn'));
   } else {
     const messageElement = form.querySelector('.form-error-message');
     if (messageElement != null) {
