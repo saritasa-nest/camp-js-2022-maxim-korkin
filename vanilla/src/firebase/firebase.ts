d4407767384f4996a2ffffbfb017b8c4476a3d38
Maxim <korkinwow@gmail.com>
2022-01-27 22:32:27 +0700

    removed deadcode #JC19-257

--- vanilla/src/firebase/firebase.ts
+++ vanilla/src/firebase/firebase.ts
@@ -14,16 +14,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-// const firebaseConfig =
-//   apiKey: process.env.VITE_API_KEY,
-//   authDomain: process.env.VITE_AUTH_DOMAIN,
-//   projectId: process.env.VITE_PROJECT_ID,
-//   storageBucket: process.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.VITE_MESSANGING_SENDER_ID,
-//   appId: process.env.VITE_APP_ID,
-//   measurementId: process.env.VITE_MEASUREMENT_ID,
-// };
-
 const app = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(app);
