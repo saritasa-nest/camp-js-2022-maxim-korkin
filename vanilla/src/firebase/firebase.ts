bbfa23c2d3579f4e99e08ee76b8b62e8ac0288dc
maxineo <maxim.korkin@saritasa.com>
2022-01-27 17:24:45 +0700

    Env variables types #JC19-257

--- vanilla/src/firebase/firebase.ts
+++ vanilla/src/firebase/firebase.ts
@@ -5,14 +5,24 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY as string,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_MESSANGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_APP_ID as string,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID as string,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSANGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+
+// const firebaseConfig =
+//   apiKey: process.env.VITE_API_KEY,
+//   authDomain: process.env.VITE_AUTH_DOMAIN,
+//   projectId: process.env.VITE_PROJECT_ID,
+//   storageBucket: process.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.VITE_MESSANGING_SENDER_ID,
+//   appId: process.env.VITE_APP_ID,
+//   measurementId: process.env.VITE_MEASUREMENT_ID,
+// };
 
 const app = initializeApp(firebaseConfig);
 
