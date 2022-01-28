f9dcd5023c3d2fafcfadc936633fef9d989d6780
maxineo <maxim.korkin@saritasa.com>
2022-01-28 12:38:20 +0700

    Film interfaces #JC19-257

---
+++ vanilla/src/interfaces/films/Film.ts
@@ -0,0 +1,16 @@
+import { FilmFields } from './FilmFields';
+
+/**
+ * Interface describing the film.
+ */
+export interface Film {
+
+  /** Fileds which describe the film. */
+  readonly filmFields: FilmFields;
+
+  /** Model in DB. */
+  readonly model: string;
+
+  /** Primary key(ID) of the film. */
+  readonly pk: number;
+}
