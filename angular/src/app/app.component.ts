79f11f4f86cf6fae0dfdbc0fb89d7fc477146a0b
maxineo <maxim.korkin@saritasa.com>
2022-02-24 09:54:21 +0700

    Removed empty css file #JC19-286

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sw-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 }
