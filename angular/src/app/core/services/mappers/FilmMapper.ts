e1bf742d617fa8cdb241141f14321b93ced4af13
Maxim <korkinwow@gmail.com>
2022-02-28 15:37:08 +0700

    Film interfaces and mapper #JC19-286

---
+++ angular/src/app/core/services/mappers/FilmMapper.ts
@@ -0,0 +1,32 @@
+import { Film } from '../../models/Film';
+
+import { FilmDto } from './dto/FilmDto/FilmDto';
+
+/**
+ * Class for mapping Film and FilmDto to each other.
+ */
+export class FilmMapper {
+  /**
+   * Method for mapping FIlmDto to Film.
+   * @param dto - FilmDto object.
+   * @returns - Film object.
+   */
+  public static fromDto(dto: FilmDto): Film {
+    const { fields } = dto;
+
+    return {
+      characterIds: fields.characters,
+      director: fields.director,
+      episodeId: fields.episode_id,
+      openingCrawl: fields.opening_crawl,
+      planetIds: fields.planets,
+      producers: fields.producer.split(', '),
+      releaseDate: new Date(fields.release_date),
+      specieIds: fields.species,
+      starshipIds: fields.starships,
+      title: fields.title,
+      vehicleIds: fields.vehicles,
+      pk: dto.pk,
+    };
+  }
+}
