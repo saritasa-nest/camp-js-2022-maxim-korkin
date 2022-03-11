4c9bb6b7760e74243575c12e76381b5fa81f0f0a
maxineo <maxim.korkin@saritasa.com>
2022-03-11 08:28:57 +0700

    Character model, dto and mapper #JC19-292

---
+++ angular/src/app/core/services/mappers/character-mapper.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+import { Character } from '../../models/Character';
+
+import { CharacterDto } from './dto/CharacterDto/CharacterDto';
+
+/**
+ * Service for mapping Character and CharacterDto to each other.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class CharacterMapperService {
+  /**
+   * Converts CharacterDto to model.
+   * @param dto - Dto object to convert.
+   */
+  public fromDto(dto: CharacterDto): Character {
+    const { fields } = dto;
+    return {
+      birthYear: fields.birth_year,
+      eyeColor: fields.eye_color,
+      gender: fields.gender,
+      hairColor: fields.hair_color,
+      height: Number(fields.height),
+      homeworldPk: fields.homeworld,
+      mass: Number(fields.mass),
+      name: fields.name,
+      skinColor: fields.skin_color,
+      pk: dto.pk,
+    };
+  }
+}
