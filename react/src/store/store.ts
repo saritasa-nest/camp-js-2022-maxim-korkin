af77662460d85171674cb72b5140a739056d072a
Maxim <korkinwow@gmail.com>
2022-03-20 20:46:26 +0700

    react init #JC19-298

---
+++ react/src/store/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  TypedUseSelectorHook, useDispatch, useSelector,
+} from 'react-redux';
+
+export const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware => getDefaultMiddleware({
+    // We need to disable this check to allow ES6 classes in Redux.
+    // You can find more info about this middleware in docs:
+    // https://redux-toolkit.js.org/api/serializabilityMiddleware
+    serializableCheck: false,
+  }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
