f9b0ad1ea7debe2a35fb2b52a6a4db181e7c4ebb
Maxim <korkinwow@gmail.com>
2022-03-21 23:00:09 +0700

    Added comments to selectors #JC19-298

--- react/src/store/auth/selectors.ts
+++ react/src/store/auth/selectors.ts
@@ -2,6 +2,8 @@ import { createSelector } from '@reduxjs/toolkit';
 
 import { RootState } from '../store';
 
+/** Selects sign in state. */
 export const selectIsSignedIn = createSelector((state: RootState) => state.auth.isSignedIn, isSignedIn => isSignedIn);
 
+/** Selects auth error. */
 export const selectAuthError = createSelector((state: RootState) => state.auth.error, error => error);
