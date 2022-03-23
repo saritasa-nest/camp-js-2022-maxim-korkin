262a60c72983413fa5dc7f468e4b55b366060d07
maxineo <maxim.korkin@saritasa.com>
2022-03-23 12:30:00 +0700

    FilmCard #JC19-298

--- react/src/features/films/pages/FilmsPage.tsx
+++ react/src/features/films/pages/FilmsPage.tsx
@@ -1,21 +1,40 @@
-import { AppBar, Box, Button } from '@mui/material';
-import { memo, VFC } from 'react';
+import {
+  AppBar, Box, Button, Stack,
+} from '@mui/material';
+import { memo, useEffect, VFC } from 'react';
 import { useDispatch } from 'react-redux';
+import { useAppSelector } from 'src/store';
 import { signOut } from 'src/store/auth/dispatchers';
+import { fetchNextPageOfFilms } from 'src/store/films/dispatchers';
+import { selectFilms } from 'src/store/films/selectors';
+import { FilmCard } from '../components/filmCard/FilmCard';
 
 const FilmsPageComponent: VFC = () => {
   const dispatch = useDispatch();
+
+  const films = useAppSelector(selectFilms);
+  // const isLoading = useAppSelector(selectIsLoading);
 
   const handleSignOut = (): void => {
     dispatch(signOut());
   };
 
+  useEffect(() => {
+    dispatch(fetchNextPageOfFilms());
+  }, [dispatch]);
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar>
-        <Button color="inherit" onClick={handleSignOut}>Sign Out</Button>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar>
+          <Button color="inherit" onClick={handleSignOut}>Sign Out</Button>
+        </AppBar>
+      </Box>
+      <Stack>
+        {films.map(film => <FilmCard film={film} />)}
+      </Stack>
+    </>
+
   );
 };
 
